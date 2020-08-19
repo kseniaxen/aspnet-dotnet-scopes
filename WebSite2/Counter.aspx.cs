6bfd658cea8ad9b971ca34af6543065db99e2027
student <student@DESKTOP-L3ET5DL>
2020-08-19 20:04:12 +0300

    get request params

--- WebSite2/Counter.aspx.cs
+++ WebSite2/Counter.aspx.cs
@@ -11,21 +11,28 @@ public partial class Counter : System.Web.UI.Page
     public int count = 0;
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        // Request Scope
+        String countString = Request.QueryString["count"];
+        if (countString != null) {
+            count = Int32.Parse(countString);
+        }
     }
 
     protected void increment_Click(object sender, EventArgs e)
     {
         // count++;
         // ViewState Scope
-        Object obj = this.ViewState["count"];
+        /* Object obj = this.ViewState["count"];
         if (obj != null)
         {
             count = (int)obj;
         }
         count += 1;
-        this.ViewState["count"] = count;
+        this.ViewState["count"] = count; */
 
+        // Request Scope
+        count++;
+        Response.Redirect($"Counter.aspx?sender={this.GetType().Name}&count={count}");
     }
 
     protected void send_Click(object sender, EventArgs e)
